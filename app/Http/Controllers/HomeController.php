362d589705ad4212d13f3a2eaab3e1bad26647d3
gnanisageable <gnaneshwar@sageabletech.com>
2016-07-15 16:18:17 +0530

    Image attribution Done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends QuintypeController {
   public function index() {
     $bulk = new Bulk();
 
-    $fields="id,headline,slug,url,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,summary,metadata,author-bio";
+    $fields="id,headline,slug,url,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,summary,metadata,hero-image-attribution";
     $bulk->addRequest('top_stories', (new StoriesRequest('top'))->addParams(["limit" => 8, "fields" => $fields]));
     $bulk->addRequest('weatherstories', (new StoriesRequest('top'))->addParams(["section" => "Weather", "limit" => 3, "fields" => $fields]));
     $bulk->addRequest('videosstories', (new StoriesRequest('top'))->addParams(["section" => "Video", "limit" => 3, "fields" => $fields]));
@@ -24,8 +24,8 @@ class HomeController extends QuintypeController {
     $bulk->execute($this->client);
 
     $a=$bulk->getResponse("top_stories");
-//echo "<pre>";
-  //  print_r($a);
+    // echo "<pre>";
+    // print_r($a);
 
     return view('home', $this->toView([
       "stories" => $bulk->getResponse("top_stories"),
@@ -40,7 +40,7 @@ class HomeController extends QuintypeController {
          $fields="id,headline,slug,url,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,summary,metadata";
         $story = $this->client->storyData(array('slug' => $slug))['story'];
 
-    //  echo "<pre>";
+     // echo "<pre>";
     // print_r($story);
         $bulk->addRequest('foodhealth', (new StoriesRequest('top'))->addParams(["section" => "Food & Health", "limit" => 3,"fields" => $fields]));
         $bulk->execute($this->client);
