ad0de47d2b99825087871ea400b051dd34d66d8e
Anagh Padmanabhan <anagh.parambath@gmail.com>
2016-08-23 18:58:24 +0530

    Add frame for new seo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,40 +8,40 @@ use App\Http\Controllers\QuintypeController;
 use App\Api\Bulk;
 use App\Api\Config;
 use App\Api\StoriesRequest;
+use Meta;
+use Quintype\Seo;
 
 class HomeController extends QuintypeController {
 
+
     public function index() {
+
+
+
         $bulk = new Bulk();
-        // $config = $this->client->config();
+        $meta = new Meta;
+        // Setting Seo meta tags
+        $config = $this->client->config();
+        $page = ["type" => "home"];
+        $home = new Seo\Home(array_merge($config, config('quintype')), $page["type"]);
+        $meta->set($home->tags());
+
         $fields = "id,headline,slug,url,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,summary,metadata,hero-image-attribution,cards,subheadline";
+
         $bulk->addRequest('top_stories', (new StoriesRequest('top'))->addParams(["limit" => 8, "fields" => $fields]));
         $bulk->addRequest('weatherstories', (new StoriesRequest('top'))->addParams(["section" => "Weather & Climate", "limit" => 3, "fields" => $fields]));
         $bulk->addRequest('videosstories', (new StoriesRequest('top'))->addParams(["section" => "Videos", "limit" => 3, "fields" => $fields]));
-
-            //   $config = $this->client->config();
-            // $sections = $config['sections'];
-
-            // $stacks= $config['stack'];
-            // echo "<pre>";
-            // print_r($stacks);
-
-    // @config.stacks.select { |stack| stack["show-on-all-sections?"] || stack["show-on-locations"].try(:include?, location) }
-
-
         $bulk->addRequest('breaking_news', (new StoriesRequest('stack-115'))->addParams(["limit" => 3, "fields" => $fields]));
-
         $bulk->addRequest('foodhealth', (new StoriesRequest('top'))->addParams(["section" => "Food & Health", "limit" => 4, "fields" => $fields]));
 
         $bulk->execute($this->client);
 
         $a = $bulk->getResponse("top_stories");
-// echo sizeof($a);
-        // echo "<pre>"; print_r($a);
 
         return view('home', $this->toView([
                     "stories" => $bulk->getResponse("top_stories"),
-                    "page" => ["type" => "home"],
+                    "page" => $page,
+                    "meta" => $meta,
                     "videos_stories" => $bulk->getResponse("videosstories"),
                     "weather_stories" => $bulk->getResponse("weatherstories"),
                     "food_storiess" => $bulk->getResponse("foodhealth"),
