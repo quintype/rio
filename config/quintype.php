64f6214349507c945e91d7bdbff66d9d77d52ecf
tripti rahangdale <imtrapti1410@gmail.com>
2016-11-21 18:27:25 +0530

    changed config

--- config/quintype.php
+++ config/quintype.php
@@ -2,9 +2,8 @@
 
 return [
     "publisher-name" => "climatedesk",
-    //"asset-host" => "http://localhost:8000",
-    "api-host" => "http://climatedesk.quintype.io",
+    "asset-host" => "http://localhost:8000",
+    "api-host" => "http://sketches.quintype.com",
     "image-cdn" => "http://quintype-01.imgix.net",
     "title" => "Climate Desk"
-
 ];
