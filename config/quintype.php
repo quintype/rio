cd3281b0093d47e0ddb8b2ab73c7c10c27d31179
tripti rahangdale <imtrapti1410@gmail.com>
2016-11-23 17:13:52 +0530

    WIP added logo and burger menu

--- config/quintype.php
+++ config/quintype.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  "publisher-name" => "climatedesk",
+  "publisher-name" => "demo1",
   "asset-host" => "http://localhost:8000",
   "api-host" => "http://sketches.quintype.com",
   "image-cdn" => "http://quintype-01.imgix.net",
-  "title" => "Climate Desk"
+  "title" => "Quintype demo"
 ];
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var util = require('gulp-quintype')
 var del = require('del');
 
-var destination = "public/climatedesk/assets";
+var destination = "public/demo1/assets";
 
 function compileAll() {
   return util.manifestAndWrite(destination, util.merge(
