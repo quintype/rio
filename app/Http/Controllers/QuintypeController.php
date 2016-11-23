a4594187abe88c00667c4c082bfabad141204791
Anagh Padmanabhan <anagh.parambath@gmail.com>
2016-11-24 00:21:18 +0530

    Integrated the API package.

--- app/Http/Controllers/QuintypeController.php
+++ app/Http/Controllers/QuintypeController.php
@@ -1,66 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+use Api;
 
-use App\Api\QuintypeClient;
-use App\Http\Controllers\Controller;
-use App\Api\Bulk;
-use App\Api\Config;
-use App\Api\StoriesRequest;
-
-class QuintypeController extends Controller {
-
-    public function __construct() {
-        $this->client = new QuintypeClient(config("quintype.api-host"));
+class QuintypeController extends Controller
+{
+    public function __construct()
+    {
+        $this->client = new Api(config('quintype.api-host'));
+        $this->config = array_merge($this->client->config(), config('quintype'));
     }
 
-    public function toView($args) {
-      return array_merge([
-        "config" => $this->config(),
-        "menuItems" => $this->menuItems(),
-        "breaking_news" => $this->getBreakingNews(),
+    public function toView($args)
+    {
+        return array_merge([
+        "config" => $this->config,
+        "client" => $this->client,
+        "nestedMenuItems" => $this->client->prepareNestedMenu($this->config["layout"]["menu"]),
+        'breaking_news' => $this->client->getBreakingNews(['limit' => 5, 'fields' => 'headline,metadata']),
       ], $args);
     }
-
-    public function getStories($args = null) {
-        return $this->client->stories($args);
-    }
-
-
-     public function relatedStories($story_id){
-        return $this->client->relatedStories($story_id);
-    }
-
-
-    public function menuItems() {
-        $config = new Config($this->client->config());
-        return $config->menuItems();
-    }
-
-    public function getBreakingNews(){
-      return $this->client->breakingNews(array('limit' => 5, "fields" => "headline,metadata"));
-    }
-
-    public function authorinfo() {
-      $config = new Config($this->client->config());
-      $abc=$config->author();
-
-      return $config->author();
-    }
-
-    public function config($args = null) {
-        return array_merge($this->client->config(), config("quintype"));
-    }
-
-    public function bulkStories($args = null) {
-        $bulk = new Bulk();
-        $bulk->addRequest('stories', (new StoriesRequest('top'))->addParams($args));
-        $bulk->execute($this->client);
-        return $bulk->getResponse("stories");
-    }
-
-    public function searchStories($args = null) {
-        return $this->client->search($args);
-    }
-
 }
