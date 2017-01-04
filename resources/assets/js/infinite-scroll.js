510bcd163aa298de17cf25b5a5a8a4cb9810592f
tripti rahangdale <imtrapti1410@gmail.com>
2017-01-04 15:02:31 +0530

    WIP infinite scroll at story page

--- resources/assets/js/infinite-scroll.js
+++ resources/assets/js/infinite-scroll.js
@@ -3,17 +3,20 @@ var template = require("./templates").story;
 
 var $doc = $(document);
 var $win = $(window);
+var firstStoryId;
+var excludeStoryIds =[];
 
-
-function nextStoryLoader(params,start,callback) {
+function nextStoryLoader(params,start,callback,excludeStoryIds) {
   params['limit'] = 1;
+  var notStoryContentIds = _.toString(excludeStoryIds);
   var fields = 'id,headline,slug,hero-image-s3-key,hero-image-metadata,first-published-at,last-published-at,alternative,published-at,author-name,author-id,sections,story-template,summary,metadata,hero-image-attribution,cards,subheadline,authors,tags';
-  $.getJSON("/api/v1/stories", _.merge({}, params, {offset: start, fields: fields}),
+  $.getJSON("/api/v1/stories", _.merge({}, params, {offset: start, fields: fields,'not-story-content-ids': notStoryContentIds}),
     (response) => callback(_.first(response["stories"])))
 }
 
 function renderStory(story) {
   var html = template.render({storyData: story});
+  excludeStoryIds.push(story.id);
   $('.js-stories-container').append(html);
 }
 
@@ -22,7 +25,7 @@ var storiesLoaded = 0;
 var scrollFn = function(e) {
   if ($doc.height() - $win.height() == $win.scrollTop()) {
     storiesLoaded += 1
-    nextStoryLoader({}, storiesLoaded, renderStory);
+    nextStoryLoader({}, storiesLoaded, renderStory,excludeStoryIds);
   }
 }
 
@@ -32,10 +35,9 @@ var scrollHandler = _.throttle(scrollFn, 300);
 $win.scroll(scrollHandler);
 
 function infiniteScroll() {
-  $('.js-story-container:last').bind('inview', function(e, isInView) {
-
-    console.log($(this).data("storySlug"), isInView);
-
+  firstStoryId = $('.js-story-header-content').data("storyId");
+  excludeStoryIds.push(firstStoryId);
+  $('.js-story-header-content:last').bind('inview', function(e, isInView) {
     if (!isInView) {
       return;
     }
@@ -43,12 +45,10 @@ function infiniteScroll() {
     var $el = $(this);
     var headline = $el.find('.js-story-headline').text();
     var url = '/' + $el.data("storySlug");
-
     if (history.replaceState) {
       history.replaceState({}, headline, url);
       $(document).prop('title', headline);
     }
-
   });
 }
 
