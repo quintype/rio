3c86d808a9bc8d748ddf730d529301e7e1b010a9
tripti rahangdale <imtrapti1410@gmail.com>
2017-05-16 15:36:33 +0530

    story preview fix

--- resources/assets/js/story_preview.js
+++ resources/assets/js/story_preview.js
@@ -1,15 +1,28 @@
 var template = require("./templates").story;
 
+function postStoryPageRender() {
+  global.app.slickSlider.photoStory();
+  global.app.slickSlider.imageGallery();
+  global.app.videos.load();
+  global.app.videos.init();
+  var tableData = $('#data-table').attr('content');
+  if (tableData) {
+    global.app.handleFileSelect(tableData);
+  }
+}
+
 module.exports = function () {
-    window.addEventListener("message", function (event) {
-        var story = event.data['story'];
-        if (story) {
-            document.getElementById("story-container").innerHTML = template.render({
-                storyData: story,
-                food_stories: Array(20).fill(story),
-                preview: true,
-                config: qtConfig
-            });
-        }
-    });
+  window.addEventListener("message", function (event) {
+    console.log('hi');
+    var story = event.data['story'];
+    if (story) {
+      document.getElementById("story-container").innerHTML = template.render({
+        storyData: story,
+        food_stories: Array(20).fill(story),
+        preview: true,
+        config: qtConfig
+      });
+      postStoryPageRender();
+    }
+  });
 }
